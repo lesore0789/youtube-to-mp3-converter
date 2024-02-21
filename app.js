d425d4a6dbab1d2d60dd546d8345d82068a95c94
Leslie Orellana <lesore0789@gmail.com>
2024-02-20 20:15:00 -0800

    Added backend stuff

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+// Express Server
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// set template engine
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+// Parse HTML data for POST request
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) =>{
+  res.send('Hello World')
+})
+// app.post("/", (req, res) =>{
+
+// }
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
